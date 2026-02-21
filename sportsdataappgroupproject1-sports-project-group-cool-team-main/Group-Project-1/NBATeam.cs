6981f2690f9e49c760ed43f506a3609dd8b5f842
Robert71287 <160978213+Robert71287@users.noreply.github.com>
2026-02-21 18:09:28 -0500

    Add files via upload
    working login

---
+++ sportsdataappgroupproject1-sports-project-group-cool-team-main/Group-Project-1/NBATeam.cs
@@ -0,0 +1,25 @@
+﻿public class NBATeam
+{
+    public string Key { get; set; }
+
+    public string Name { get; set; }
+
+    public string City { get; set; }
+
+    public string Conference { get; set; }
+
+    public string Division { get; set; }
+
+    public NBAStadiumDetails StadiumDetails { get; set; }
+}
+
+public class NBAStadiumDetails
+{
+    public string Name { get; set; }
+
+    public string City { get; set; }
+
+    public string State { get; set; }
+
+    public int Capacity { get; set; }
+}
