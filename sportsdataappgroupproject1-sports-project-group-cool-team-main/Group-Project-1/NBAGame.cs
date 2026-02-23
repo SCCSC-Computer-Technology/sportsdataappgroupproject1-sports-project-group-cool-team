974c02e03de01642d1bc00440f631c0baf092a70
Robert71287 <160978213+Robert71287@users.noreply.github.com>
2026-02-23 18:41:10 -0500

    Add files via upload
    all api and added login page authethication

---
+++ sportsdataappgroupproject1-sports-project-group-cool-team-main/Group-Project-1/NBAGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Group_Project_1
+{
+
+    public class NBAGame
+    {
+        public int GameID { get; set; }
+        public int Season { get; set; }
+        public int SeasonType { get; set; }   // 1=Regular, 2=Pre, 3=Post 
+
+        public string Status { get; set; } = "";
+
+        public DateTime? DateTime { get; set; }
+
+        public string AwayTeam { get; set; } = "";
+        public string HomeTeam { get; set; } = "";
+
+        public int? AwayTeamScore { get; set; }
+        public int? HomeTeamScore { get; set; }
+
+        public string Channel { get; set; } = "";
+    }
+}
